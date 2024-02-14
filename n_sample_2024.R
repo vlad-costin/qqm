83f4b1461f837ad50f64712e15a6080f77ae603c
vladcostin <55540328+vlad-costin@users.noreply.github.com>
2024-02-14 16:00:20 +0000

    Add files via upload

---
+++ n_sample_2024.R
@@ -0,0 +1,20 @@
+
+if(!require(tidyverse)){
+  install.packages("tidyverse")
+}
+
+data <- readr::read_csv("https://raw.githubusercontent.com/vlad-costin/qqm/main/qqm2024_labreport.csv") 
+
+# candidate number as seed
+RNGkind(sample.kind = "Rejection")
+set.seed(cand_no)
+
+
+#random subset of rows - between 100 and 250 smaller than the whole dataset
+data <- data[sample(c(1:nrow(data)), 
+                    size = (nrow(data) - sample(100:250, 1))),]
+data
+
+# cand_no <- 123
+# source("https://raw.githubusercontent.com/ra328research/qqm/main/rename_n_sample.R")
+# data
